409617d2bc7e4afcbeec0e72a92f2bd7d17f253f
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-08-17 19:15:58 +0900

    moved the project here

---
+++ todo-firebase/src/components/TodoForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <b-container>
+      <b-input-group>
+        <b-form-input type="text" v-model="value"/>
+        <b-input-group-append>
+          <b-button variant="success" @click="handlerAddTodo()">送信</b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </b-container>
+</template>
+
+<script>
+  export default {
+    name: 'TodoForm',
+    data() {
+      return {
+        value: '',
+      };
+    },
+    methods: {
+      handlerAddTodo() {
+        this.$emit('handleParentAddTodo', this.value);
+        this.value = '';
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
--- todo-firebase/src/router/index.js
+++ todo-firebase/src/router/index.js
@@ -1,14 +1,23 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import Index from '@/pages/Index';
 
-Vue.use(VueRouter);
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Index',
+    component: Index
   },
   {
     path: '/about',
@@ -16,14 +25,14 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-];
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
